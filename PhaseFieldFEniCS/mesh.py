e388cc76c7abf66d8a1f1e6fb05eb732938bdedb
Vahid Ziaei Rad <vahid.ziaei@uni.lu>
2019-05-29 17:29:00 +0430

    Naga

--- PhaseFieldFEniCS/mesh.py
+++ PhaseFieldFEniCS/mesh.py
@@ -23,36 +23,36 @@ meshname="fracking_hsize%g" % (hsize)
 
 # Generate a XDMF/HDF5 based mesh from a Gmsh string
 geofile = \
-		"""
-			lc = DefineNumber[ %g, Name "Parameters/lc" ];
-			H = 1.;
-			L = 1.;
+        """
+            lc = DefineNumber[ %g, Name "Parameters/lc" ];
+            H = 1.;
+            L = 1.;
 
-			a=0.25;
+            a=0.25;
 
-		    Point(1) = {-L/2, H/2, 0, 5*lc};
-		    Point(2) = {L/2, H/2, 0, 5*lc};
-		    Point(3) = {L/2, -H/2, 0, 5*lc};
-		    Point(4) = {-L/2, -H/2, 0, 5*lc};
+            Point(1) = {-L/2, H/2, 0, 5*lc};
+            Point(2) = {L/2, H/2, 0, 5*lc};
+            Point(3) = {L/2, -H/2, 0, 5*lc};
+            Point(4) = {-L/2, -H/2, 0, 5*lc};
 
-		    Point(5) = {-a, 0, 0, 0.1*lc};
-		    Point(6) = {a, 0, 0, 0.1*lc};
+            Point(5) = {-a, 0, 0, 0.1*lc};
+            Point(6) = {a, 0, 0, 0.1*lc};
 
 
-		    Line(1) = {1, 2};
-		    Line(2) = {2, 3};
-		    Line(3) = {3, 4};
-		    Line(4) = {4, 1};
-		    Line Loop(5) = {1, 2, 3, 4};
+            Line(1) = {1, 2};
+            Line(2) = {2, 3};
+            Line(3) = {3, 4};
+            Line(4) = {4, 1};
+            Line Loop(5) = {1, 2, 3, 4};
 
-			Plane Surface(30) = {5};
+            Plane Surface(30) = {5};
 
-			Line(6) = {5, 6};
-		    Line{6} In Surface{30};
+            Line(6) = {5, 6};
+            Line{6} In Surface{30};
 
-			Physical Surface(1) = {30};
+            Physical Surface(1) = {30};
 
-			Physical Line(101) = {6};
+            Physical Line(101) = {6};
 
 """%(hsize)
 
@@ -63,48 +63,48 @@ _mesh  = Mesh() #creat empty mesh object
 
 if not os.path.isfile(subdir + meshname + ".xdmf"):
         if MPI.comm_world.rank == 0:
-        	# Create temporary .geo file defining the mesh
-        	if os.path.isdir(subdir) == False:
-        		os.mkdir(subdir)
-        	fgeo = open(subdir + meshname + ".geo", "w")
-        	fgeo.writelines(geofile)
-        	fgeo.close()
-        	# Calling gmsh and dolfin-convert to generate the .xml mesh (as well as a MeshFunction file)
-        	try:
-                	subprocess.call(["gmsh", "-2", "-o", subdir + meshname + ".msh", subdir + meshname + ".geo"])
-        	except OSError:
-                	print("-----------------------------------------------------------------------------")
-                	print(" Error: unable to generate the mesh using gmsh")
-                	print(" Make sure that you have gmsh installed and have added it to your system PATH")
-                	print("-----------------------------------------------------------------------------")
+            # Create temporary .geo file defining the mesh
+            if os.path.isdir(subdir) == False:
+                os.mkdir(subdir)
+            fgeo = open(subdir + meshname + ".geo", "w")
+            fgeo.writelines(geofile)
+            fgeo.close()
+            # Calling gmsh and dolfin-convert to generate the .xml mesh (as well as a MeshFunction file)
+            try:
+                    subprocess.call(["gmsh", "-2", "-o", subdir + meshname + ".msh", subdir + meshname + ".geo"])
+            except OSError:
+                    print("-----------------------------------------------------------------------------")
+                    print(" Error: unable to generate the mesh using gmsh")
+                    print(" Make sure that you have gmsh installed and have added it to your system PATH")
+                    print("-----------------------------------------------------------------------------")
 
 
-        	meshconvert.convert2xml(subdir + meshname + ".msh", subdir + meshname + ".xml", "gmsh")
+            meshconvert.convert2xml(subdir + meshname + ".msh", subdir + meshname + ".xml", "gmsh")
 
-        	# Convert to XDMF
-        	MPI.barrier(MPI.comm_world)
-        	mesh = Mesh(subdir + meshname + ".xml")
-        	XDMF = XDMFFile(MPI.comm_world, subdir + meshname + ".xdmf")
-        	XDMF.write(mesh)
-        	XDMF.read(_mesh)
-		
+            # Convert to XDMF
+            MPI.barrier(MPI.comm_world)
+            mesh = Mesh(subdir + meshname + ".xml")
+            XDMF = XDMFFile(MPI.comm_world, subdir + meshname + ".xdmf")
+            XDMF.write(mesh)
+            XDMF.read(_mesh)
+        
         if os.path.isfile(subdir + meshname + "_physical_region.xml") and os.path.isfile(subdir + meshname + "_facet_region.xml"):
-		if MPI.comm_world.rank == 0:
-                	mesh = Mesh(subdir + meshname + ".xml")
-                	subdomains = MeshFunction("size_t", mesh, subdir + meshname + "_physical_region.xml")
-                	boundaries = MeshFunction("size_t", mesh, subdir + meshname + "_facet_region.xml")
-                	HDF5 = HDF5File(MPI.comm_world, subdir + meshname + "_physical_facet.h5", "w")
-                	HDF5.write(mesh, "/mesh")
-                	HDF5.write(subdomains, "/subdomains")
-                	HDF5.write(boundaries, "/boundaries")
-                	print("Finish writting physical_facet to HDF5")
+            if MPI.comm_world.rank == 0:
+                    mesh = Mesh(subdir + meshname + ".xml")
+                    subdomains = MeshFunction("size_t", mesh, subdir + meshname + "_physical_region.xml")
+                    boundaries = MeshFunction("size_t", mesh, subdir + meshname + "_facet_region.xml")
+                    HDF5 = HDF5File(MPI.comm_world, subdir + meshname + "_physical_facet.h5", "w")
+                    HDF5.write(mesh, "/mesh")
+                    HDF5.write(subdomains, "/subdomains")
+                    HDF5.write(boundaries, "/boundaries")
+                    print("Finish writting physical_facet to HDF5")
 
         print("Mesh completed")
 
     # Read the mesh if existing
 else:
         XDMF = XDMFFile(MPI.comm_world, subdir + meshname + ".xdmf")
-		
+        
         XDMF.read(_mesh)
 
 
