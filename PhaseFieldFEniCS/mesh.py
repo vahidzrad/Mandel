b5e8243e0848f84c8a8ee87431ea8a8442a16a30
vahidzrad <vahidzrad@gmail.com>
2019-05-29 17:16:39 +0430

    Update mesh.py

--- PhaseFieldFEniCS/mesh.py
+++ PhaseFieldFEniCS/mesh.py
@@ -62,47 +62,48 @@ _mesh  = Mesh() #creat empty mesh object
 
 
 if not os.path.isfile(subdir + meshname + ".xdmf"):
-        #if MPI.rank(mpi_comm_world()) == 0:
-        # Create temporary .geo file defining the mesh
-        if os.path.isdir(subdir) == False:
-            os.mkdir(subdir)
-        fgeo = open(subdir + meshname + ".geo", "w")
-        fgeo.writelines(geofile)
-        fgeo.close()
-        # Calling gmsh and dolfin-convert to generate the .xml mesh (as well as a MeshFunction file)
-        try:
-                subprocess.call(["gmsh", "-2", "-o", subdir + meshname + ".msh", subdir + meshname + ".geo"])
-        except OSError:
-                print("-----------------------------------------------------------------------------")
-                print(" Error: unable to generate the mesh using gmsh")
-                print(" Make sure that you have gmsh installed and have added it to your system PATH")
-                print("-----------------------------------------------------------------------------")
+        if MPI.comm_world.rank == 0:
+        	# Create temporary .geo file defining the mesh
+        	if os.path.isdir(subdir) == False:
+        		os.mkdir(subdir)
+        	fgeo = open(subdir + meshname + ".geo", "w")
+        	fgeo.writelines(geofile)
+        	fgeo.close()
+        	# Calling gmsh and dolfin-convert to generate the .xml mesh (as well as a MeshFunction file)
+        	try:
+                	subprocess.call(["gmsh", "-2", "-o", subdir + meshname + ".msh", subdir + meshname + ".geo"])
+        	except OSError:
+                	print("-----------------------------------------------------------------------------")
+                	print(" Error: unable to generate the mesh using gmsh")
+                	print(" Make sure that you have gmsh installed and have added it to your system PATH")
+                	print("-----------------------------------------------------------------------------")
 
 
-        meshconvert.convert2xml(subdir + meshname + ".msh", subdir + meshname + ".xml", "gmsh")
+        	meshconvert.convert2xml(subdir + meshname + ".msh", subdir + meshname + ".xml", "gmsh")
 
-        # Convert to XDMF
-        MPI.barrier(MPI.comm_world)
-        mesh = Mesh(subdir + meshname + ".xml")
-        XDMF = XDMFFile(subdir + meshname + ".xdmf")
-        XDMF.write(mesh)
-        XDMF.read(_mesh)
+        	# Convert to XDMF
+        	MPI.barrier(MPI.comm_world)
+        	mesh = Mesh(subdir + meshname + ".xml")
+        	XDMF = XDMFFile(MPI.comm_world, subdir + meshname + ".xdmf")
+        	XDMF.write(mesh)
+        	XDMF.read(_mesh)
 
         if os.path.isfile(subdir + meshname + "_physical_region.xml") and os.path.isfile(subdir + meshname + "_facet_region.xml"):
-                mesh = Mesh(subdir + meshname + ".xml")
-                subdomains = MeshFunction("size_t", mesh, subdir + meshname + "_physical_region.xml")
-                boundaries = MeshFunction("size_t", mesh, subdir + meshname + "_facet_region.xml")
-                HDF5 = HDF5File(MPI.comm_world, subdir + meshname + "_physical_facet.h5", "w")
-                HDF5.write(mesh, "/mesh")
-                HDF5.write(subdomains, "/subdomains")
-                HDF5.write(boundaries, "/boundaries")
-                print("Finish writting physical_facet to HDF5")
+		if MPI.comm_world.rank == 0:
+                	mesh = Mesh(subdir + meshname + ".xml")
+                	subdomains = MeshFunction("size_t", mesh, subdir + meshname + "_physical_region.xml")
+                	boundaries = MeshFunction("size_t", mesh, subdir + meshname + "_facet_region.xml")
+                	HDF5 = HDF5File(MPI.comm_world, subdir + meshname + "_physical_facet.h5", "w")
+                	HDF5.write(mesh, "/mesh")
+                	HDF5.write(subdomains, "/subdomains")
+                	HDF5.write(boundaries, "/boundaries")
+                	print("Finish writting physical_facet to HDF5")
 
         print("Mesh completed")
 
     # Read the mesh if existing
 else:
-        XDMF = XDMFFile(subdir + meshname + ".xdmf")
+        XDMF = XDMFFile(MPI.comm_world, subdir + meshname + ".xdmf")
 		
         XDMF.read(_mesh)
 
